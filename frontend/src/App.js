ec2034464a2fc28c0c0fea0d85ef6a7281fb9f21
Alp ÖZASLAN <alp.ozaslan07@gmail.com>
2022-06-06 23:22:26 +0300

    Frontend skelaton was created.

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div>Appbar</div>
+      <div>Logo</div>
+      <div>search bar</div>
+      <section>
+        <p>Your Books</p>
+        <span>see all {">"} </span>
+        <div>
+          <div>Book 1</div>
+          <div>Book 2</div>
+          <div>Book 3</div>
+          <div>Book 4</div>
+          <div>Book 5</div>
+        </div>
+      </section>
+      <section>
+        <p>Popular Books</p>
+        <span>see all {">"} </span>
+        <div>
+          <div>Book 1</div>
+          <div>Book 2</div>
+          <div>Book 3</div>
+          <div>Book 4</div>
+          <div>Book 5</div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
